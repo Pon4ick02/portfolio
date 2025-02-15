a5f376f0742b47f5439392f654c79f933e2ae9cc
Pon4ick <krpon4ick@gmail.com>
2025-02-15 15:48:52 +0200

    -1st part of main changes to blog and portfolio -almost done with bd -work with admin files

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import Blog
 
-# Create your views here.
+
+def all_blogs(request):
+    blogs = Blog.objects.order_by('-date')[:5]
+    return render(request, 'blog/all_blogs.html', {'blogs': blogs})
+
+
+"""
+from django.shortcuts import render
+from .models import Project
+
+def home(request):
+    projects = Project.objects.all()
+    return render(request, 'portfolio/home.html', {'projects': projects})
+
+"""
