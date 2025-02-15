1be088c1010239fabbf1adfb5440c4d458135d09
Pon4ick <krpon4ick@gmail.com>
2025-02-15 18:27:27 +0200

    finish work + work on htmls

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 class Blog(models.Model):
     title = models.CharField(max_length=200)  # Заголовок
-    title_url = models.URLField(blank=True)  # Ссылка
     date = models.DateField(default=None, blank=True, null=True)
-    description = models.CharField(max_length=250)
+    description = models.TextField()
 
+    def __str__(self):
+        return self.title
